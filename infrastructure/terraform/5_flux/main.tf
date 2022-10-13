db7ff106de70d1b9f4820dfe64e943b70c6214b8
Julius Daniel Herrera Glomm <julius.daniel.herrera.glomm@gmail.com>
2022-10-13 22:45:48 +0200

    chore: fix dependency

--- infrastructure/terraform/5_flux/main.tf
+++ infrastructure/terraform/5_flux/main.tf
@@ -169,7 +169,7 @@ locals {
 resource "kubectl_manifest" "install" {
   for_each = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [
-    kubernetes_namespace.namespaces["flux-system"],
+    kubernetes_namespace.namespaces,
   ]
   yaml_body = each.value
 }
@@ -177,7 +177,7 @@ resource "kubectl_manifest" "install" {
 resource "kubectl_manifest" "sync" {
   for_each = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
   depends_on = [
-    kubernetes_namespace.namespaces["flux-system"],
+    kubernetes_namespace.namespaces,
   ]
   yaml_body = each.value
 }
