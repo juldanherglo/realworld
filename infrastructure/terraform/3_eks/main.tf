35d7d360db6c0412ec8a60e24ecc0a2d460eafee
Julius Daniel Herrera Glomm <julius.daniel.herrera.glomm@gmail.com>
2022-10-14 09:00:54 +0200

    chore: tfsec ignore

--- infrastructure/terraform/3_eks/main.tf
+++ infrastructure/terraform/3_eks/main.tf
@@ -17,6 +17,7 @@ locals {
 #tfsec:ignore:aws-eks-no-public-cluster-access
 #tfsec:ignore:aws-eks-no-public-cluster-access-to-cidr
 #tfsec:ignore:aws-ec2-no-public-egress-sgr
+#tfsec:ignore:aws-eks-enable-control-plane-logging
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 18.0"
