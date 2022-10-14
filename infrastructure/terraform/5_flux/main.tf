ac7ff8f9d1fe27c03a5b6ca3e670d73ecedbfa4d
Julius Daniel Herrera Glomm <julius.daniel.herrera.glomm@gmail.com>
2022-10-14 09:28:02 +0200

    chore: fix github files

--- infrastructure/terraform/5_flux/main.tf
+++ infrastructure/terraform/5_flux/main.tf
@@ -112,22 +112,25 @@ resource "github_repository_deploy_key" "main" {
 }
 
 resource "github_repository_file" "install" {
-  repository = var.repository_name
-  file       = data.flux_install.main.path
-  content    = data.flux_install.main.content
-  branch     = var.branch
+  repository          = var.repository_name
+  file                = data.flux_install.main.path
+  content             = data.flux_install.main.content
+  branch              = var.branch
+  overwrite_on_create = true
 }
 
 resource "github_repository_file" "sync" {
-  repository = var.repository_name
-  file       = data.flux_sync.main.path
-  content    = data.flux_sync.main.content
-  branch     = var.branch
+  repository          = var.repository_name
+  file                = data.flux_sync.main.path
+  content             = data.flux_sync.main.content
+  branch              = var.branch
+  overwrite_on_create = true
 }
 
 resource "github_repository_file" "kustomize" {
-  repository = var.repository_name
-  file       = data.flux_sync.main.kustomize_path
-  content    = data.flux_sync.main.kustomize_content
-  branch     = var.branch
+  repository          = var.repository_name
+  file                = data.flux_sync.main.kustomize_path
+  content             = data.flux_sync.main.kustomize_content
+  branch              = var.branch
+  overwrite_on_create = true
 }
